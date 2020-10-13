a518d21820ad1732401a9d5051441f512973965d
micantia <saylos14082000@gmail.com>
2020-10-13 19:20:12 +0300

    currentDayNumber property and day(for:) function added

--- GoalApp/Screens/Goal/GoalViewModel.swift
+++ GoalApp/Screens/Goal/GoalViewModel.swift
@@ -22,13 +22,27 @@ final class GoalViewModel {
         return try? context.fetch(request).first
     }
     
+    func day(for dayNumber: Int) -> Day? {
+        guard let goalCreationDate = provider.userSettings.goalCreationDate else { return nil }
+        let startOfGoalCreationDate = Calendar.current.startOfDay(for: goalCreationDate)
+        guard let dateForDayNumber = Calendar.current.date(byAdding: .day, value: dayNumber, to: startOfGoalCreationDate) else { return nil }
+        let request: NSFetchRequest<Day> = Day.fetchRequest()
+        request.predicate = NSPredicate(format: "date == %@", dateForDayNumber as NSDate)
+        let context = provider.coreData.persistanceContainer.viewContext
+        return try? context.fetch(request).first
+    }
+    
+    var currentDayNumber: Int {
+        guard let goalCreationDate = provider.userSettings.goalCreationDate else { return 1 }
+        return Date().daysDifference(with: goalCreationDate)
+    }
+    
     var availableDaysNumber: [DayCellModel] {
         guard let goalCreationDate = provider.userSettings.goalCreationDate,
               let goalDeadline = provider.userSettings.goalDeadline else { return [] }
         
-        let calendar = Calendar.current
-        let dayDifference = calendar.dateComponents([.day], from: goalCreationDate, to: goalDeadline).day ?? 1
+        let dayDifference = goalDeadline.daysDifference(with: goalCreationDate) + 1
         
-        return (1...dayDifference).map { DayCellModel(dayNumber: $0, isEnabled: false) }
+        return (1...dayDifference).map { DayCellModel(dayNumber: $0, isEnabled: true) }
     }
 }

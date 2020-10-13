66a97ffe7e73767f2fbb5a4d079245e5065a513b
micantia <saylos14082000@gmail.com>
2020-10-13 17:50:23 +0300

    filling goalCreationDate on first launch

--- GoalApp/Supporting Files/AppDelegate.swift
+++ GoalApp/Supporting Files/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let userSettings = AppServices.default.userSettings
+        
+        if userSettings.goalCreationDate == nil {
+            userSettings.goalCreationDate = Date()
+        }
         return true
     }
 

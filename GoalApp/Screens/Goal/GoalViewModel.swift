062092977cf1ad5a10a5bb7bfdff525d8da3dac3
micantia <saylos14082000@gmail.com>
2020-10-13 17:51:10 +0300

    provider updated + availableDaysNumber property added

--- GoalApp/Screens/Goal/GoalViewModel.swift
+++ GoalApp/Screens/Goal/GoalViewModel.swift
@@ -8,9 +8,9 @@
 import CoreData
 
 final class GoalViewModel {
-    let provider: CoreDataStackProvider
+    let provider: CoreDataStackProvider & UserSettingsProvider
     
-    init(provider: CoreDataStackProvider) {
+    init(provider: CoreDataStackProvider & UserSettingsProvider) {
         self.provider = provider
     }
     
@@ -20,5 +20,15 @@ final class GoalViewModel {
         request.predicate = NSPredicate(format: "date == %@", currentDay as NSDate)
         let context = provider.coreData.persistanceContainer.viewContext
         return try? context.fetch(request).first
+    }
+    
+    var availableDaysNumber: [DayCellModel] {
+        guard let goalCreationDate = provider.userSettings.goalCreationDate,
+              let goalDeadline = provider.userSettings.goalDeadline else { return [] }
+        
+        let calendar = Calendar.current
+        let dayDifference = calendar.dateComponents([.day], from: goalCreationDate, to: goalDeadline).day ?? 1
+        
+        return (1...dayDifference).map { DayCellModel(dayNumber: $0, isEnabled: false) }
     }
 }
